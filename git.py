dde60bea3d96cd6e67f9deefd8705b303c0073b7
Hades0413 <migvel.gomero.1901@hotmail.com>
2024-09-22 19:05:38 -0500

    Cargando...

--- git.py
+++ git.py
@@ -5,13 +5,23 @@ import random
 año = 2024
 mes = 9  # Octubre
 
+# Asegúrate de que tu rama local esté actualizada
+os.system('git pull origin main')
+
 for i in range(17):
     d = str(i) + ' days ago'
     # El día se calculará aleatoriamente en el mes de octubre
     rand_day = random.randint(1, 31)
+    
     with open('test.txt', 'a') as file:
         file.write(d + '\n')
-    os.system(f'git add test.txt')
+        
+    os.system('git add test.txt')
     os.system(f'git commit --date="{año}-{mes:02d}-{rand_day:02d} 17:51:00 -0500" -m "Commit {i}"')
-os.system('git push -u origin main')
 
+# Intenta hacer push
+push_result = os.system('git push -u origin main')
+
+# Verifica si el push fue exitoso
+if push_result != 0:
+    print("El push falló. Revisa si hay conflictos.")
