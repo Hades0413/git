8864d0f354404138dccea7cdbafe4ea0c56cacba
Hades0413 <migvel.gomero.1901@hotmail.com>
2024-09-22 19:08:16 -0500

    Cargando

--- git.py
+++ git.py
@@ -5,20 +5,16 @@ import random
 año = 2024
 mes = 9  # Octubre
 
-for i in range(100):
+for i in range(17):
     d = str(i) + ' days ago'
     # El día se calculará aleatoriamente en el mes de octubre
     rand_day = random.randint(1, 31)
-    
     with open('test.txt', 'a') as file:
         file.write(d + '\n')
-        
-    os.system('git add test.txt')
+    os.system(f'git add test.txt')
     os.system(f'git commit --date="{año}-{mes:02d}-{rand_day:02d} 17:51:00 -0500" -m "Commit {i}"')
+os.system('git push -u origin main')
 
-# Intenta hacer push
-push_result = os.system('git push -u origin main')
-
-# Verifica si el push fue exitoso
-if push_result != 0:
-    print("El push falló. Revisa si hay conflictos.")
+#git commit --amend --no-edit --date="Fri Nov 6 20:00:00 2015 -0600"
+#git fetch origin master
+#git rebase origin/master
